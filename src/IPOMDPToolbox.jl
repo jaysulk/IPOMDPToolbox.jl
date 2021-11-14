d066c3e1fb995e0bc958b7dfaedbd8254c614545
Jason A. Sulskis <2976461+jaysulk@users.noreply.github.com>
2021-11-14 17:25:17 -0600

    Update IPOMDPToolbox.jl

--- src/IPOMDPToolbox.jl
+++ src/IPOMDPToolbox.jl
@@ -21,6 +21,6 @@ export
     include("interactivebelief.jl")
     include("gpomdp.jl")
     include("reductionsolver.jl")
-    include("ipomdpstoolbox.jl")
+    include("ipomdptoolbox.jl")
     include("functions.jl")
 end
