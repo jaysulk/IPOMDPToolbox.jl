baf0045c410e7a469d31831e29bb4fbab8ab6068
Jason A. Sulskis <jason.sulskis@gmail.com>
2022-10-24 18:38:00 -0700

    Update IPOMDPToolbox.jl

--- src/IPOMDPToolbox.jl
+++ src/IPOMDPToolbox.jl
@@ -1,5 +1,38 @@
 module IPOMDPToolbox
 
-greet() = print("Hello World!")
+using POMDPs
+using IPOMDPs
+using SARSOP
+using POMDPModelTools
+using BeliefUpdaters
+import Base: (==)
+export
+    pomdpModel,
+    ipomdpModel,
 
-end # module IPOMDPToolbox
+    printPOMDP,
+
+    DiscreteInteractiveBelief,
+    DiscreteInteractiveUpdater,
+
+    ReductionSolver,
+    ReductionPolicy,
+
+    IBPISolver,
+    IBPIPolicy,
+
+    #temporary
+    BPIPolicy,
+    solve_fresh!,
+    continue_solving,
+    print_solver_stats,
+    load_policy,
+    IBPIsimulate
+
+    include("interactivebelief.jl")
+    include("gpomdp.jl")
+    include("reductionsolver.jl")
+    include("ibpisolver.jl")
+    include("ipomdpstoolbox.jl")
+    include("functions.jl")
+end
